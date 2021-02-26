400d3279ec4e55e61a112b72a2d247830ea7863f
miodrag.vukovic <35942671+miodragvukovic@users.noreply.github.com>
2021-02-26 14:58:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ jQuery(document).ready(function($){
     itemSelector: '.product',
     layoutMode: 'fitRows',
   });
-
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
   $('.filter-item').click(function(e) {
     e.preventDefault();
     $grid.isotope({ filter: $(this).attr('data-filter') });
